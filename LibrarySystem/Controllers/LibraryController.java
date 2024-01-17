f6c171629c9d037cea2fac6f8fc604fb2308d1d4
adi <adityavshedge3101@gmail.com>
2024-01-17 16:14:33 +0530

    MVC Refactor: Moved Filed into LibrarySystem

--- LibrarySystem/Controllers/LibraryController.java
+++ LibrarySystem/Controllers/LibraryController.java
@@ -1,78 +1,12 @@
+
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
-class Library {
 
-    List<Book> books = new ArrayList<>();
-    List<User> users = new ArrayList<>();
-
-    void addBook(Book b) {
-        books.add(b);
-    }
-
-    void addUser(User user) {
-        users.add(user);
-    }
-
-    List<Book> getBooks() {
-
-        if (books.isEmpty()) {
-            System.out.println("Library is Empty");
-        }
-
-        return books;
-    }
-
-    Book getBook(String bookName) {
-
-        for (Book book : books) {
-            if (book.getBookName().equals(bookName)) {
-                return book;
-            }
-        }
-        return null;
-    }
-
-    User getUser(String userName) {
-
-        for (User user : users) {
-            if (user.getUsername().equals(userName)) {
-                return user;
-            }
-        }
-        return null;
-    }
-
-    Book issueBook(String username, String bookname) {
-        User user = getUser(username);
-        Book book = getBook(bookname);
-
-        if (book == null || user == null)
-            return null;
-
-        if (user.canBorrow() && book.isBookAvailable()) {
-            return book;
-        }
-
-        return null;
-    }
-
-    void showUserBooks(String username) {
-        for (User user : users) {
-            if (user.getUsername().equals(username)) {
-                for(Book book: user.getUserBooks()) {
-                    System.out.println("Books Borrowed " + book.getBookName());
-                }
-            }
-
-        }
-    }
-
-}
-
-class LibrarySystem {
+class LibraryController {
     public static void main(String[] args) {
         // Initialise the library
         Library library = new Library();
@@ -112,6 +46,7 @@ class LibrarySystem {
                     username = sc.nextLine();
 
                     User user = library.getUser(username);
+                    
 
                     if (user.borrowBook(library, bookName)) {
                         System.out.println("Book Borrowed");
