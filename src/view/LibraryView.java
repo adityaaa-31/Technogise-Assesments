c08c4bfaefd11d2b0a3568abb07f334b23e4a8d6
adi <adityavshedge3101@gmail.com>
2024-01-18 11:40:23 +0530

    Created View: Created view for showing all library books

--- src/view/LibraryView.java
+++ src/view/LibraryView.java
@@ -3,12 +3,19 @@ package view;
 import java.util.List;
 
 import model.Book;
+import model.Library;
 
 public class LibraryView {
 
     public static void showUserBooks(List<Book> books) {
         for (int i = 0; i < books.size(); i++) {
             System.out.println("Books Borrowed " + books.get(i).getBookName());
+        }
+    }
+
+    public static void showAllBooks(Library library) {
+        for (Book book : library.books) {
+            System.out.println(book.getBookName());
         }
     }
 }
