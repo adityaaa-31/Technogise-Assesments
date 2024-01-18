8b108fcb9c99952655f28b6cedbdfba15ffb9071
adi <adityavshedge3101@gmail.com>
2024-01-18 14:43:50 +0530

    Updated Book and User Model: Added uuid for both

--- src/model/Book.java
+++ src/model/Book.java
@@ -1,12 +1,16 @@
 package model;
 
+import java.util.UUID;
+
 public class Book {
     private String bookName;
     private String authorName;
+    private UUID bookId;
 
     public Book(String bookName, String authorName, int noOfCopies) {
         this.bookName = bookName;
         this.authorName = authorName;
+        this.bookId = UUID.randomUUID();
     }
 
     public String getBookName() {
@@ -17,6 +21,9 @@ public class Book {
         return authorName;
     }
 
+    public UUID getBookId() {
+        return bookId;
+    }
 
     public void setBookName(String bookName) {
         this.bookName = bookName;
--- src/model/User.java
+++ src/model/User.java
@@ -2,18 +2,25 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class User {
     private String username;
+    UUID userId;
 
     public List<Book> borrowedBooks = new ArrayList<>();
 
     public User(String username) {
         this.username = username;
+        this.userId = UUID.randomUUID();
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public UUID getUserId() {
+        return userId;
     }
 
     public void setUsername(String username) {
