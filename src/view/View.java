7e9541ef070036d26dd09751de715f108c26f118
adi <adityavshedge3101@gmail.com>
2024-01-18 14:49:15 +0530

    Updated View: Removed show user book method

--- src/view/View.java
+++ src/view/View.java
@@ -1,22 +1,4 @@
 package view;
 
-import java.util.List;
-
-import model.Book;
-import model.User;
-import model.Library;
-
-public class View extends Library{
-
-    // public void showUserBooks(String username) {
-    //     for (User user : users) {
-    //         if (user.getUsername().equals(username)) {
-    //             List<Book> books = user.getUserBooks();
-    //             for (int i = 0; i < books.size(); i++) {
-    //                 System.out.println("Books Borrowed " + books.get(i).getBookName());
-    //             }
-    //         }
-
-    //     }
-    // }
+public class View {
 }
