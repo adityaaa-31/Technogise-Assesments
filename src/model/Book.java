67ba459d0a94a727742b82e6fbed670baa7291af
adi <adityavshedge3101@gmail.com>
2024-01-18 14:37:21 +0530

    Updated Book Model: Removed number of copies variable

--- src/model/Book.java
+++ src/model/Book.java
@@ -3,12 +3,10 @@ package model;
 public class Book {
     private String bookName;
     private String authorName;
-    private int noOfCopies;
 
     public Book(String bookName, String authorName, int noOfCopies) {
         this.bookName = bookName;
         this.authorName = authorName;
-        this.noOfCopies = noOfCopies;
     }
 
     public String getBookName() {
@@ -19,9 +17,6 @@ public class Book {
         return authorName;
     }
 
-    public int getNoOfCopies() {
-        return noOfCopies;
-    }
 
     public void setBookName(String bookName) {
         this.bookName = bookName;
@@ -29,10 +24,6 @@ public class Book {
 
     public void setAuthorName(String authorName) {
         this.authorName = authorName;
-    }
-
-    public void setNoOfCopies(int noOfCopies) {
-        this.noOfCopies = noOfCopies;
     }
 
 }
