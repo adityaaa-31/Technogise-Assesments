5aa950136a3ec9438c1b4a5d495ba25d88b741c0
adi <adityavshedge3101@gmail.com>
2024-01-19 11:59:44 +0530

    Renamed Database Repository file to DatabaseConfig

--- src/repository/DatabaseConfig.java
+++ src/repository/DatabaseConfig.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseRepository {
+public class DatabaseConfig {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/library";
     private static final String USER = "root";
     private static final String PASSWORD = "mysqlroot";
@@ -32,3 +32,4 @@ public class DatabaseRepository {
         }
     }
 }
+    
